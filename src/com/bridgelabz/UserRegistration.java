9a5fc25ed11f1c7d32bc38c903cbd9d5aac8a169
adithya-6337 <veldandiadithya337@gmail.com>
2022-11-14 13:36:24 +0530

    password minimum 8 character

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -7,21 +7,21 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Mobile no : ");
-        String mobileNum = scanner.nextLine();
-        checkMobileNumber(mobileNum);
+        System.out.print("Enter password minimum 8 characters : ");
+        String password = scanner.nextLine();
+        checkPasswordRuleOne(password);
     }
 
-    private static void checkMobileNumber(String mobilNum) {
-        String regex = "^[0-9]{2}+[ ][0-9]{10}$";
+    private static void checkPasswordRuleOne(String mobilNum) {
+        String regex = "^[a-zA-Z]{8,}";
 
         Pattern patternCheck = Pattern.compile(regex);
         Matcher matcherCheck = patternCheck.matcher(mobilNum);
 
         if (matcherCheck.matches()) {
-            System.out.println("Valid Mobile Number");
+            System.out.println("Valid Password");
         } else {
-            System.out.println("Invalid Mobile Number");
+            System.out.println("Invalid Password");
         }
     }
 }
